e8a1d6709b1e5225e08971a3ad25c0e9b6c40939
Felipe Paul Martins <felipe.paulmartins@hotmail.com>
2014-04-27 13:44:00 +0200

    Gestion du Keep Alive, TODO gérer un timeout

--- socket.c
+++ socket.c
@@ -261,6 +261,7 @@ void process_connection(int sock)
 {
 
 	char ipcli[TAILLE_READ_BUFFER];
+	int keepAlive;
 
 	// Affichage de l'adresse IP du serveur local.
 	fprintf(stdout, "Connexion : locale   ");
@@ -270,9 +271,14 @@ void process_connection(int sock)
 	fprintf(stdout, "\t    distante ");
 	print_socket_address(sock, DISTANT, ipcli);
 
-	// Traite la requête HTTP
-	if (processHttp(sock, ipcli) != 1)
-		close(sock); // Fermeture de la socket si non KEEP-ALIVE
+	do
+	{
+		// Traite la requête HTTP
+		keepAlive = processHttp(sock, ipcli);
+	} while (keepAlive);
+
+	// Fermeture de la socket
+	close(sock);
 }
 
 /*
